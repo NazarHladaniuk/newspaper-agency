b5e1ceb7e2a8bcf8931ae3991dc341bedcc83a23
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-03-29 16:02:10 +0300

    add test main page

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(
+        "<html>"
+        "<h1>Test newspaper</h1>"
+        "</html>"
+    )
